3a88da07579511bba28df34a024d7f9e6cdd41ee
Suizo, Rowena G <143061861+Ahsnd@users.noreply.github.com>
2023-11-27 20:26:56 +0800

    Add files via upload

---
+++ User.js
@@ -0,0 +1,182 @@
+export const users = [
+  {
+    id: 1,
+    name: {
+      firstname: "Jungkook",
+      middlename: "",
+      lastname: "Kim",
+    },
+    address: "Seoul, South Korea",
+  },
+  {
+    id: 2,
+    name: {
+      firstname: "Lisa",
+      middlename: "",
+      lastname: "Manoban",
+    },
+    address: "Busan, South Korea",
+  },
+  {
+    id: 3,
+    name: {
+      firstname: "Taeyong",
+      middlename: "",
+      lastname: "Lee",
+    },
+    address: "Incheon, South Korea",
+  },
+  {
+    id: 4,
+    name: {
+      firstname: "Jisoo",
+      middlename: "",
+      lastname: "Kim",
+    },
+    address: "Jeju Island, South Korea",
+  },
+  {
+    id: 5,
+    name: {
+      firstname: "Jimin",
+      middlename: "",
+      lastname: "Park",
+    },
+    address: "Gwangju, South Korea",
+  },
+  {
+    id: 6,
+    name: {
+      firstname: "IU",
+      middlename: "",
+      lastname: "Lee",
+    },
+    address: "Daejeon, South Korea",
+  },
+  {
+    id: 7,
+    name: {
+      firstname: "Baekhyun",
+      middlename: "",
+      lastname: "Byun",
+    },
+    address: "Suwon, South Korea",
+  },
+  {
+    id: 8,
+    name: {
+      firstname: "Rosé",
+      middlename: "",
+      lastname: "Park",
+    },
+    address: "Ulsan, South Korea",
+  },
+  {
+    id: 9,
+    name: {
+      firstname: "Suga",
+      middlename: "",
+      lastname: "Min",
+    },
+    address: "Daegu, South Korea",
+  },
+  {
+    id: 10,
+    name: {
+      firstname: "Yeri",
+      middlename: "",
+      lastname: "Kim",
+    },
+    address: "Changwon, South Korea",
+  },
+  {
+    id: 11,
+    name: {
+      firstname: "Chanyeol",
+      middlename: "",
+      lastname: "Park",
+    },
+    address: "Jeonju, South Korea",
+  },
+  {
+    id: 12,
+    name: {
+      firstname: "Momo",
+      middlename: "",
+      lastname: "Hirai",
+    },
+    address: "Jecheon, South Korea",
+  },
+  {
+    id: 13,
+    name: {
+      firstname: "Jackson",
+      middlename: "",
+      lastname: "Wang",
+    },
+    address: "Goyang, South Korea",
+  },
+  {
+    id: 14,
+    name: {
+      firstname: "Joy",
+      middlename: "",
+      lastname: "Park",
+    },
+    address: "Yeosu, South Korea",
+  },
+  {
+    id: 15,
+    name: {
+      firstname: "RM",
+      middlename: "",
+      lastname: "Kim",
+    },
+    address: "Pohang, South Korea",
+  },
+  {
+    id: 16,
+    name: {
+      firstname: "Solar",
+      middlename: "",
+      lastname: "Kim",
+    },
+    address: "Jeju City, South Korea",
+  },
+  {
+    id: 17,
+    name: {
+      firstname: "Jin",
+      middlename: "",
+      lastname: "Kim",
+    },
+    address: "Yangsan, South Korea",
+  },
+  {
+    id: 18,
+    name: {
+      firstname: "Jennie",
+      middlename: "",
+      lastname: "Kim",
+    },
+    address: "Gimhae, South Korea",
+  },
+  {
+    id: 19,
+    name: {
+      firstname: "Mark",
+      middlename: "",
+      lastname: "Tuan",
+    },
+    address: "Ansan, South Korea",
+  },
+  {
+    id: 20,
+    name: {
+      firstname: "Hayoung",
+      middlename: "",
+      lastname: "Oh",
+    },
+    address: "Chuncheon, South Korea",
+  },
+];
